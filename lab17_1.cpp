fcd968f80a44dcea7244cac142d57077fb4c9d59
fusenattawat <141806694+fusenattawat@users.noreply.github.com>
2024-02-06 14:42:56 +0700

    lab17_1.cpp

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -3,30 +3,30 @@ using namespace std;
 
 int main(){
     int nA,nB;
-    int *A,*B,**C; // Change C to pointer to pointer
+    int *A,*B,**C; 
 	
     cout << "Length of A: ";
     cin >> nA;
     
-    A = new int[nA]; // Allocate memory for array A
+    A = new int[nA]; 
     cout << "Input Array A: ";
     for(int i=0; i < nA; i++) cin >> A[i];
     
     cout << "Length of B: ";
     cin >> nB;
     
-    B = new int[nB]; // Allocate memory for array B
+    B = new int[nB]; 
     cout << "Input Array B: ";
     for(int i=0; i < nB; i++) cin >> B[i];
     
-    C = new int*[nA]; // Allocate memory for array of pointers for C
+    C = new int*[nA]; 
     for(int i=0; i < nA; i++) {
-        C[i] = new int[nB]; // Allocate memory for each row of C
+        C[i] = new int[nB]; 
     }
     
     for(int i=0; i < nA; i++){
         for(int j=0; j < nB; j++) {
-            C[i][j] = A[i]*B[j]; // Store the product in correct position of C
+            C[i][j] = A[i]*B[j]; 
         }
     }
     
